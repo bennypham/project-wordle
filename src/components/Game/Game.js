88a919b902b5aba657bb580460321a2c4d972991
bennypham <benny.npham@gmail.com>
2025-01-23 23:30:08 -0800

    highligh cells if incorrect, correct, or misplaced

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -8,13 +8,14 @@ import { sample } from '../../utils';
 import { WORDS } from '../../data';
 
 // Pick a random word on every pageload.
-// const answer = sample(WORDS);
-const answer = 'hello';
+const answer = sample(WORDS);
+// const answer = 'HELLO';
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
 function Game() {
 	const [guesses, setGuesses] = React.useState([]);
+	const [gameStatus, setGameStatus] = React.useState(false);
 
 	// Update guesses array with tempGuessWord
 	function handleSubmitGuesses(tempGuessWord) {
@@ -24,7 +25,7 @@ function Game() {
 
 	return (
 		<div>
-			<RenderGuess guesses={guesses}></RenderGuess>
+			<RenderGuess guesses={guesses} answer={answer}></RenderGuess>
 			<GuessInput handleSubmitGuesses={handleSubmitGuesses} />
 		</div>
 	);
